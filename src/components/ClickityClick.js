040e775bf9b61b3e81bdeac1d8f817671709bb13
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-04-07 20:54:39 +1000

    Button toggles between clicked and not clicked

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,32 @@
 // Code ClickityClick Component Here
+import React from 'react';
+
+export default class ClickityClick extends React.Component {
+    constructor() {
+        super();
+    
+
+//Define the initial state:
+    this.state = {
+        hasBeenClicked: false,
+    }
+
+    }
+    handleClick = (event) => {
+        event.preventDefault();
+        this.setState (previousState => {
+            return {
+                hasBeenClicked: !previousState.hasBeenClicked,
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+                <button onClick={this.handleClick}>Click Me!</button>
+            </div>
+        )
+    }
+}
